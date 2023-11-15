6d6098be264412438b75018a9a4beb1cea2aad07
jorgelaramedina <jorge.lara.la@gmail.com>
2023-11-15 13:43:38 +0100

    estructura de proyecto

--- p11-project7-group2/src/App.js
+++ p11-project7-group2/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import { Route,Routes } from "react-router-dom";
+import  {SearchBar}  from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <header>  
-      </header>
+      <Routes>
+        <Route element={<SearchBar />} />
+      </Routes>
     </div>
   );
 }
