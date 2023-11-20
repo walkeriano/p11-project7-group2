8902ca25b62fb696096f7f30ab76b06a63e65d7d
jorgelaramedina <jorge.lara.la@gmail.com>
2023-11-20 12:06:42 +0100

    profilepage y route

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/HomePage";
+import ProfilePage from "./pages/Profile/ProfilePage";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/profile-page" element={<ProfilePage/>} />
     </Routes>
   );
 }
