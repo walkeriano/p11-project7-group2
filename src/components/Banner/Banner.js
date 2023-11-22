09ffc9d1fd30e92bd3b69193d035d2fbfad122e4
Alexander  Walker <awalkerbarreda@gmail.com>
2023-11-22 03:53:16 -0500

    page detalle receta

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,33 +1,33 @@
-import logotipo from '../../assets/img/logotipo.svg';
-import facebook from '../../assets/img/facebook.svg';
-import tiktok from '../../assets/img/tiktok.svg';
-import instagram from '../../assets/img/instagram.svg';
-import banner from '../../assets/img/banner.jpg';
-import './Banner.css';
+import logotipo from "../../assets/img/logotipo.svg";
+import facebook from "../../assets/img/facebook.svg";
+import tiktok from "../../assets/img/tiktok.svg";
+import instagram from "../../assets/img/instagram.svg";
+import banner from "../../assets/img/banner.jpg";
+import "./Banner.css";
 
 export default function Logotipo() {
   return (
-    <div>
-      <div className='bodyheader'>
-        <div className='info'>
-          <img className='logotipo' src={logotipo} alt="Chef yourself" />
-          <p className='slogan'>Explora, comparte<span> y disfruta</span></p>
-          <div className='socialNetworks'>
-            <button className='button'>Iniciar Sesión</button>
-            <aside>
-              <img className='socialApp' src={facebook} alt="Logotipo Facebook" />
-              <img className='socialApp' src={tiktok} alt="Logotipo Tik Tok" />
-              <img className='socialApp' src={instagram} alt="Logotipo Instagram" />
-            </aside>
-          </div>
+    <section className="bodyheader">
+      <div className="info">
+        <img className="logotipo" src={logotipo} alt="Chef yourself" />
+        <p className="slogan">
+          Explora,<br/>comparte<span>y disfruta</span>
+        </p>
+        <div className="socialNetworks">
+          <button className="button">Iniciar Sesión</button>
+          <aside>
+            <img className="socialApp" src={facebook} alt="Logotipo Facebook" />
+            <img className="socialApp" src={tiktok} alt="Logotipo Tik Tok" />
+            <img
+              className="socialApp"
+              src={instagram}
+              alt="Logotipo Instagram"
+            />
+          </aside>
         </div>
-        <img className='imagen' src={banner} alt="" />
-        <div className='degradado'></div>
       </div>
-    </div>
-
-  )
+      <img className="imagen" src={banner} alt="" />
+      <div className="degradado"></div>
+    </section>
+  );
 }
-
-
-
