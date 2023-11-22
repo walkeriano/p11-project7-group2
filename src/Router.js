745c60cedd0affc87a73be1343f6c40b1cf68250
AndresOcampoFactoriaF5 <less.broken.hearts@outlook.com>
2023-11-22 09:48:34 +0100

    Login changes

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/HomePage";
 import InfoRecipes from "./pages/InfoRecipes/InfoRecipesPage";
+import Acces from "./pages/Acces/AccesPage";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/acces" element={<Acces/>} />
       <Route path="/detalles-recetas/:id" element={<InfoRecipes/>} />
     </Routes>
   );
--- src/components/CreateAccount/CreateAccount.jsx
+++ src/components/CreateAccount/CreateAccount.jsx
@@ -1,9 +1,60 @@
-import "./CreateAccount.css"
+import "../CreateAccount/CreateAccount.css";
+import photoIcon from "../../assets/img/photoIcon.png";
+import { useState } from "react";
 
 export default function CreateAccount() {
+    const [perfilphoto, setPerfilphoto] = useState(null);
+    const handleimage = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPerfilphoto(reader.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setPerfilphoto(null);
+        }
+
+    };
+
     return (
-       <div>
-        
-       </div> 
+        <section className="body-sesion">
+            <section className="create-account">
+                <div className="login-photo">
+                    <div
+                        className="photo-circle"
+                        onClick={() => document.querySelector(".input-image").click()}
+                    >
+                        <input className="input-image" hidden onChange={handleimage} name="perfilphoto" id="profile" type="file" accept="image/png" />
+
+                        {perfilphoto ? (
+                            <img className="image-on" src={perfilphoto} alt="perfil photo" />
+                        ) : (
+                            <img className="image-of" src={photoIcon} alt="photo icon" />
+                        )}
+                    </div>
+                    <div className="notification">
+                        <span></span>
+                        <p>Adjuntar Imagen</p>
+                    </div>
+                </div>
+                <div className="cont-form-create">
+                    <label htmlFor="nombre">
+                        <input placeholder="Nombre" id="nombre" className="inp-credential" name="nombre" type="text" />
+                    </label>
+                    <label htmlFor="email">
+                        <input placeholder="Correo" id="email" className="inp-credential" name="email" type="email" />
+                    </label>
+                    <label htmlFor="password">
+                        <input placeholder="Crear contraseña" id="password" className="inp-credential" name="password" type="password" />
+                    </label>
+                </div>
+            </section>
+            <div>
+                <p><a href=""></a></p>
+                <button className="submit-btn" name="submit" type="submit" >CREAR CUENTA</button>
+            </div>
+        </section>
     );
 }
