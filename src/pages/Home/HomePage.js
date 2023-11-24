a384f13137a2c6ac2db63240d40f84f75da7a456
ccm24 <113595661+ccm24@users.noreply.github.com>
2023-11-24 02:52:33 -0800

    footer

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import ListRecipes from '../../components/ListRecipes/ListRecipes'
 import Banner from '../../components/BannerHome/Banner'
 import ListCategories from '../../components/ListCategories/ListCategories'
+import Footer from '../../components/Footer/Footer'
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Banner />    
       <ListCategories />
       <ListRecipes />
+      <Footer />
     </section>
   )
 }
