a9400b823722729ed1e10830bb528d6fc73c33f2
Alexander  Walker <awalkerbarreda@gmail.com>
2023-11-24 05:03:32 -0500

    profile jorge ready + integration page

--- src/components/UserAdvice/UserAdvice.jsx
+++ src/components/UserAdvice/UserAdvice.jsx
@@ -1,28 +1,19 @@
 import "./UserAdvice.css";
 
-
 export default function UserAdvice() {
-    return (
-
-        <section className='cont-useradvice'>
-            <div className='cont-advice'>
-                <p className='ms'>
-                    En hora buena jorge!<br /> <br /><br />
-                    Gracias por compartir tus recetas y hacer más grande esta comunidad!
-                </p>
-            </div>
-            <button className='btn-terminar'>
-                Terminar
-            </button>
-            <div className='ms-finaladvice'>
-                <p className='ms1'>
-                    Explora, comparte
-                </p>
-                <p className='ms2'>
-                    y disfruta
-                </p>
-            </div>
-        </section>
-
-    );
+  return (
+    <section className="cont-useradvice">
+      <div className="cont-advice">
+        <h5>En hora buena!</h5>
+        <p>
+          Gracias por compartir tus recetas y hacer más grande esta comunidad!
+        </p>
+      </div>
+      <button className="btn-terminar">Terminar</button>
+      <div className="ms-finaladvice">
+        <p className="ms1">Explora, comparte</p>
+        <p className="ms2">y disfruta</p>
+      </div>
+    </section>
+  );
 }
