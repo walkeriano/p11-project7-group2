0da330122a9c60788e370c6f83163917532c82fa
jorgelaramedina <jorge.lara.la@gmail.com>
2023-11-30 01:35:38 +0100

    carrusel estilos

--- src/components/ContinentCarrusel/ContinentCarrusel.jsx
+++ src/components/ContinentCarrusel/ContinentCarrusel.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './ContinentCarrusel.css';
 import { Link } from 'react-router-dom';
-
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const ContinentCarrusel = () => {
   const [indiceActual, setIndiceActual] = useState(0);
   const [continentes, setContinentes] = useState([]);
   const [loading, setLoading] = useState(true);
+  let sliderContainer = useRef(null);
 
   useEffect(() => {
     const url = "http://localhost:5000/continentes";
@@ -26,26 +27,43 @@ const ContinentCarrusel = () => {
         console.error("Error", error.message);
         setLoading(false);
       });
-  }, []); 
+  }, []);
 
-  const moverCarrusel = (direccion) => {
-    const nuevoIndice = (indiceActual + direccion + continentes.length) % continentes.length;
-    setIndiceActual(nuevoIndice);
+  const left = () => {
+    if (sliderContainer.current) {
+      sliderContainer.current.scrollLeft -= 500;
+    }
+  };
+  const right = () => {
+    if (sliderContainer.current) {
+      sliderContainer.current.scrollLeft += 500;
+    }
   };
 
+
   return (
-    <div className="carrusel-container">
-      {loading && <p>Cargando...</p>}
-      {!loading && (
-        <div className="carrusel-wrapper">
-          {continentes.map((continente, index) => (
-            <Link key={continente.id} to={`/continentes`} className={`continentes ${index === indiceActual ? 'activa' : ''}`}>
-              <img className='imagen-carrusel' src={continente.imagen} alt={continente.nombre} />
-              <h2>{continente.nombre}</h2>
-            </Link>
-          ))}
-        </div>
-      )}
+    <div className='cont-carrusel' >
+
+      <section className="carrusel-container" ref={sliderContainer}>
+        {loading && <p>Cargando...</p>}
+        {!loading && (
+          <div className="carrusel-wrapper">
+            {continentes.map((continente, index) => (
+              <Link key={continente.id} to={`/continentes`} className={`continentes ${index === indiceActual ? 'activa' : ''}`}>
+                <img className='imagen-carrusel' src={continente.imagen} alt={continente.nombre} />
+                <h2><span>{continente.nombre}</span></h2>
+              </Link>
+            ))}
+
+          </div>
+        )}
+      </section>
+
+      <div className='btns'>
+        <button className='arrowButton1' onClick={left}>{'<'}</button>
+        <button className='arrowButton2' onClick={right}>{'>'}</button>
+      </div>
+
     </div>
   );
 };
